677f56988341b04d96b4495f2823e5bc2e2c9430
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-28 14:11:25 +0530

    bfs start

--- hello/testbfs.py
+++ hello/testbfs.py
@@ -1,4 +1,18 @@
+
+
 grid=[[1,1,0,1],
       [1,0,0,1],
       [0,0,0,0]]
 
+
+row = len(grid)
+col = len(grid[0])
+count = 0
+
+for i in range(row):
+    for j in range(col):
+        if(grid[i][j]==1):
+            bfs((i,j))
+            count += 1
+
+print("The number of island present is the world: ",count)
